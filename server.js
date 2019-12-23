20bd3a0fac72900e793233e80db7f6d63004812c
Tamara Golub <tgolub89@gmail.com>
2019-12-23 10:43:26 -0500

    removed 2 npms

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
+
 
 const PORT = process.env.PORT || 3000;
 
